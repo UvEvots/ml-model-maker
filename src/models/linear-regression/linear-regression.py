cb4ed018905548c499e151d1b7b4bcb43014d777
Steve Vu <uvevots@Steves-Laptop.local>
2026-09-16 10:11:49 -0400

    Add data loading

--- src/models/linear-regression/linear-regression.py
+++ src/models/linear-regression/linear-regression.py
@@ -1,4 +1,14 @@
+from pathlib import Path
 import numpy as np
+import pandas as pd
+
+# load data
+PROJECT_ROOT = Path(__file__).resolve().parents[3]
+data_path = PROJECT_ROOT / "data" / "housing_prices.csv"
+
+df = pd.read_csv(data_path)
+
+print(df.head())
 
 #HYPERPARAMETERS
 learningRate = 0.01
