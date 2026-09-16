653ebe101e2cfea224ce3a4cb6b99e83b2540517
Steve Vu <uvevots@Steves-Laptop.local>
2026-09-16 10:51:51 -0400

    Add MSE and epoch loops

--- src/models/linear-regression/linear-regression.py
+++ src/models/linear-regression/linear-regression.py
@@ -8,10 +8,30 @@ data_path = PROJECT_ROOT / "data" / "housing_prices.csv"
 
 df = pd.read_csv(data_path)
 
-print(df.head())
-
 #HYPERPARAMETERS
 learningRate = 0.01
 batchSize = 64
 Epochs = 25
+
+# PARAMETERS - STARTING POINT
+weight = 0
+bias = 0
+
+# Normalizing features
+feature = df["square_feet"].to_numpy()
+normalized = (feature - feature.mean() / feature.std())
+
+df["square_feet"] = normalized
+
+for epoch in range(Epochs):
+    batch = df.sample(batchSize)
+
+    feature = batch["square_feet"].to_numpy()
+    predictions = feature * weight + bias
+
+    label = batch["price_thousands"].to_numpy()
+
+    mse = np.mean((label - predictions) ** 2)
+    print(mse)
+
 
